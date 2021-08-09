24e135eb334e0baa407657a6f29f3f06e1875189
Sam Hermes <samhermes@me.com>
2021-08-09 15:02:19 -0500

    Update accordion animation, update styles for when javascript is disabled

--- src/js/components/accordion.js
+++ src/js/components/accordion.js
@@ -10,13 +10,48 @@ accordions.forEach((accordion, index) => {
     button.setAttribute('aria-controls', 'accordion-' + index)
     button.setAttribute('aria-expanded', 'false')
     button.appendChild(headingChildren)
-
     heading.appendChild(button)
     
     content.setAttribute('aria-controlledby', 'accordion-' + index)
 
     button.addEventListener('click', function() {
-        accordion.classList.toggle('is-active')
-        this.setAttribute('aria-expanded', 'true')
+        const expanded = this.getAttribute('aria-expanded');
+        expanded === 'true' ? this.setAttribute('aria-expanded', 'false') : this.setAttribute('aria-expanded', 'true');
+
+        if (expanded === 'true') {
+            collapseSection(content)
+
+            accordion.addEventListener('transitionend', function(e) {
+                accordion.removeEventListener('transitionend', arguments.callee);
+                accordion.classList.remove('is-active');
+            });
+        } else {
+            expandSection(content)
+            accordion.classList.add('is-active')
+        }
     });
 });
+
+const collapseSection = (element) => {
+    const sectionHeight = element.scrollHeight;
+    const elementTransition = element.style.transition;
+    element.style.transition = '';
+
+    requestAnimationFrame(function() {
+        element.style.height = sectionHeight + 'px';
+        element.style.transition = elementTransition;
+        requestAnimationFrame(function() {
+            element.style.height = 0 + 'px';
+        });
+    });
+}
+
+const expandSection = (element) => {
+    const sectionHeight = element.scrollHeight;
+    
+    element.style.height = sectionHeight + 'px';
+    element.addEventListener('transitionend', function(e) {
+        element.removeEventListener('transitionend', arguments.callee);
+        element.style.height = 'auto';
+    });
+}
