47ee700e0cd76bbc60226e7f2f6226242b73af8f
Sam Hermes <samhermes@me.com>
2021-08-21 19:04:13 -0500

    Break accordion function apart

--- src/js/components/accordion.js
+++ src/js/components/accordion.js
@@ -1,50 +1,60 @@
 const accordions = document.querySelectorAll('.accordion')
 
 accordions.forEach((accordion, index) => {
-    const heading = accordion.querySelector('.accordion-heading')
-    const content = accordion.querySelector('.accordion-content')
-    const headingChildren = accordion.querySelector('.accordion-heading > *')
+    const elements = {
+        heading: accordion.querySelector('.accordion-heading'),
+        content: accordion.querySelector('.accordion-content'),
+        headingChildren: accordion.querySelector('.accordion-heading > *')
+    }
 
     const button = document.createElement('button')
     button.setAttribute('type', 'button')
     button.setAttribute('aria-controls', 'accordion-' + index)
     button.setAttribute('aria-expanded', 'false')
-    button.appendChild(headingChildren)
-    heading.appendChild(button)
+    button.appendChild(elements.headingChildren)
+    elements.heading.appendChild(button)
     
-    content.setAttribute('aria-controlledby', 'accordion-' + index)
+    elements.content.setAttribute('aria-controlledby', 'accordion-' + index)
 
     button.addEventListener('click', () => {
         const expanded = button.getAttribute('aria-expanded')
         expanded === 'true' ? button.setAttribute('aria-expanded', 'false') : button.setAttribute('aria-expanded', 'true')
 
         if (expanded === 'true') {
-            const sectionHeight = content.scrollHeight
-            const elementTransition = content.style.transition
-            content.style.transition = ''
-
-            requestAnimationFrame(() => {
-                content.style.height = sectionHeight + 'px'
-                content.style.transition = elementTransition
-                requestAnimationFrame(() => {
-                    content.style.height = 0 + 'px'
-                })
-            })
-
-            accordion.addEventListener('transitionend', function(){
-                accordion.removeEventListener('transitionend', arguments.callee)
-                accordion.classList.remove('is-active')
-            })
+            closeAccordion(accordion, elements)
         } else {
-            const sectionHeight = content.scrollHeight
-    
-            content.style.height = sectionHeight + 'px'
-            content.addEventListener('transitionend', function(){
-                content.removeEventListener('transitionend', arguments.callee)
-                content.style.height = 'auto'
-            })
-
-            accordion.classList.add('is-active')
+            openAccordion(accordion, elements)   
         }
     })
 })
+
+const openAccordion = (accordion, elements) => {
+    const sectionHeight = elements.content.scrollHeight
+    
+    elements.content.style.height = sectionHeight + 'px'
+    elements.content.addEventListener('transitionend', function(){
+        elements.content.removeEventListener('transitionend', arguments.callee)
+        elements.content.style.height = 'auto'
+    })
+
+    accordion.classList.add('is-active')
+}
+
+const closeAccordion = (accordion, elements) => {
+    const sectionHeight = elements.content.scrollHeight
+    const elementTransition = elements.content.style.transition
+    elements.content.style.transition = ''
+
+    requestAnimationFrame(() => {
+        elements.content.style.height = sectionHeight + 'px'
+        elements.content.style.transition = elementTransition
+        requestAnimationFrame(() => {
+            elements.content.style.height = 0 + 'px'
+        })
+    })
+
+    accordion.addEventListener('transitionend', function(){
+        accordion.removeEventListener('transitionend', arguments.callee)
+        accordion.classList.remove('is-active')
+    })
+}
