6624345226963d409048f33d0978aa7fb1b6169d
Sam Hermes <samhermes@me.com>
2024-05-22 22:30:03 -0500

    Display accordion content when printed

--- src/js/components/accordion.js
+++ src/js/components/accordion.js
@@ -1,9 +1,9 @@
 /**
  * Component Name: Accordions
  * Version: 1.0.4
- * 
+ *
  * Collapse a content area under a heading.
- * 
+ *
  * @param {string} selector Class name or ID of accordion elements.
  */
 
@@ -12,7 +12,7 @@ export const Accordions = (selector = '.accordion') => {
     const accordions = document.querySelectorAll(selector)
 
     // If no accordions are found, return.
-    if ( !accordions ) {
+    if (!accordions) {
         return
     }
 
@@ -24,7 +24,7 @@ export const Accordions = (selector = '.accordion') => {
                 content: accordion.querySelector('.accordion-content'),
                 headingChildren: accordion.querySelector('.accordion-heading > *').innerHTML
             }
-        
+
             const button = document.createElement('button')
             button.setAttribute('type', 'button')
             button.setAttribute('aria-controls', 'accordion-content-' + index)
@@ -33,43 +33,43 @@ export const Accordions = (selector = '.accordion') => {
             button.innerHTML = elements.headingChildren
             elements.heading.innerHTML = ''
             elements.heading.appendChild(button)
-            
+
             elements.content.setAttribute('id', 'accordion-content-' + index)
             elements.content.setAttribute('aria-labelledby', 'accordion-button-' + index)
             elements.content.setAttribute('role', 'region')
-        
+
             button.addEventListener('click', () => {
                 const expanded = button.getAttribute('aria-expanded')
                 expanded === 'true' ? button.setAttribute('aria-expanded', 'false') : button.setAttribute('aria-expanded', 'true')
-        
+
                 if (expanded === 'true') {
                     closeAccordion(accordion, elements)
                 } else {
-                    openAccordion(accordion, elements)   
+                    openAccordion(accordion, elements)
                 }
             })
-        })   
+        })
     }
 
     // Open the accordion, update heading and content.
     const openAccordion = (accordion, elements) => {
         const contentHeight = elements.content.scrollHeight
-        
+
         elements.content.style.height = contentHeight + 'px'
         elements.content.addEventListener('transitionend', function() {
             elements.content.style.height = 'auto'
         }, { once: true })
-    
+
         accordion.classList.add('is-active')
     }
-    
+
     // Close the accordion, update heading and content.
     const closeAccordion = (accordion, elements) => {
         const reducedMotion = window.matchMedia('(prefers-reduced-motion: reduce)')
         const contentHeight = elements.content.scrollHeight
         const elementTransition = elements.content.style.transition
         elements.content.style.transition = ''
-    
+
         requestAnimationFrame(() => {
             elements.content.style.height = contentHeight + 'px'
             elements.content.style.transition = elementTransition
@@ -77,8 +77,8 @@ export const Accordions = (selector = '.accordion') => {
                 elements.content.style.height = 0 + 'px'
             })
         })
-    
-        if ( reducedMotion.matches ) {
+
+        if (reducedMotion.matches) {
             // Remove class immediately.
             accordion.classList.remove('is-active')
         } else {
